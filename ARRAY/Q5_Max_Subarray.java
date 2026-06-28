8074ff025c904e9674d2fad79cf4dcce06f13d4d
ngnandita <gargayannandita356@gmail.com>
2026-06-28 14:03:09 +0530

    Added

--- ARRAY/Q5_Max_Subarray.java
+++ ARRAY/Q5_Max_Subarray.java
@@ -1,5 +1,24 @@
-package ARRAY;
-
-public class Q5_Max_Subarray {
-    
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Initialize maxSoFar with the lowest possible value 
+        // to handle arrays with all negative numbers.
+        int maxSoFar = nums[0];
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            // Update the maximum sum found so far
+            if (currentSum > maxSoFar) {
+                maxSoFar = currentSum;
+            }
+            
+            // If current sum drops below 0, reset it
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSoFar;
+    }
 }
